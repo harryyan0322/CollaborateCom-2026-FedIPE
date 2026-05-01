29ddec61de41019b6e8c9c058cbe25535ba39b95
harryyan0322 <129665662+harryyan0322@users.noreply.github.com>
2026-05-02 02:48:19 +0800

    Add core FedIPE method implementation

---
+++ data/__init__.py
@@ -0,0 +1,5 @@
+"""Dataset helpers for FedIPE."""
+
+from data.federated_platforms import CORE_SEMANTIC_ANCHORS, FederatedPlatformBuilder, PlatformGraphData
+
+__all__ = ["CORE_SEMANTIC_ANCHORS", "FederatedPlatformBuilder", "PlatformGraphData"]
