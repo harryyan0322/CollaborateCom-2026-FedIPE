38e7924d168bd0231bd2e993625ad50c20f3b761
harryyan0322 <129665662+harryyan0322@users.noreply.github.com>
2026-05-02 02:48:21 +0800

    Add core FedIPE method implementation

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+"""Model exports for FedIPE."""
+
+from models.fedipe_model import ClientForwardOutput, FedIPEClientModel, SHARED_PARAMETER_PREFIXES
+
+__all__ = ["ClientForwardOutput", "FedIPEClientModel", "SHARED_PARAMETER_PREFIXES"]
